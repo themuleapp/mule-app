f4a74848447693ac54fe2bb3a8b7c9858f735561
Mrigank Doshy <mrigankdoshy@gmail.com>
2020-06-05 10:39:00 -0400

    Code organization

--- Code/mule/lib/Screens/Menu/drawer_user_controller.dart
+++ Code/mule/lib/Screens/Menu/drawer_user_controller.dart
@@ -35,10 +35,22 @@ class _DrawerUserControllerState extends State<DrawerUserController> with Ticker
 
   @override
   void initState() {
-    animationController = AnimationController(duration: const Duration(milliseconds: 2000), vsync: this);
-    iconAnimationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 0));
-    iconAnimationController..animateTo(1.0, duration: const Duration(milliseconds: 0), curve: Curves.fastOutSlowIn);
-    scrollController = ScrollController(initialScrollOffset: widget.drawerWidth);
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 2000),
+        vsync: this
+    );
+    iconAnimationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 0)
+    );
+    iconAnimationController..animateTo(
+        1.0,
+        duration: const Duration(milliseconds: 0),
+        curve: Curves.fastOutSlowIn
+    );
+    scrollController = ScrollController(
+        initialScrollOffset: widget.drawerWidth
+    );
     scrollController
       ..addListener(() {
         if (scrollController.offset <= 0) {
@@ -50,10 +62,18 @@ class _DrawerUserControllerState extends State<DrawerUserController> with Ticker
               } catch (_) {}
             });
           }
-          iconAnimationController.animateTo(0.0, duration: const Duration(milliseconds: 0), curve: Curves.fastOutSlowIn);
-        } else if (scrollController.offset > 0 && scrollController.offset < widget.drawerWidth) {
-          iconAnimationController.animateTo((scrollController.offset * 100 / (widget.drawerWidth)) / 100,
-              duration: const Duration(milliseconds: 0), curve: Curves.fastOutSlowIn);
+          iconAnimationController.animateTo(
+              0.0,
+              duration: const Duration(milliseconds: 0),
+              curve: Curves.fastOutSlowIn
+          );
+        } else if (scrollController.offset > 0
+            && scrollController.offset < widget.drawerWidth) {
+          iconAnimationController.animateTo(
+              (scrollController.offset * 100 / (widget.drawerWidth)) / 100,
+              duration: const Duration(milliseconds: 0),
+              curve: Curves.fastOutSlowIn
+          );
         } else if (scrollController.offset <= widget.drawerWidth) {
           if (scrolloffset != 0.0) {
             setState(() {
@@ -63,7 +83,11 @@ class _DrawerUserControllerState extends State<DrawerUserController> with Ticker
               } catch (_) {}
             });
           }
-          iconAnimationController.animateTo(1.0, duration: const Duration(milliseconds: 0), curve: Curves.fastOutSlowIn);
+          iconAnimationController.animateTo(
+              1.0,
+              duration: const Duration(milliseconds: 0),
+              curve: Curves.fastOutSlowIn
+          );
         }
       });
     WidgetsBinding.instance.addPostFrameCallback((_) => getInitState());
@@ -89,99 +113,124 @@ class _DrawerUserControllerState extends State<DrawerUserController> with Ticker
           child: SizedBox(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width + widget.drawerWidth,
-            //we use with as screen width and add drawerWidth (from navigation_home_screen)
+            /* we use with as screen width and add drawerWidth
+            (from navigation_home_screen)*/
             child: Row(
               children: <Widget>[
-            SizedBox(
-            width: widget.drawerWidth,
-              //we divided first drawer Width with HomeDrawer and second full-screen Width with all home screen, we called screen View
-              height: MediaQuery.of(context).size.height,
-              child: AnimatedBuilder(
-                animation: iconAnimationController,
-                builder: (BuildContext context, Widget child) {
-                  return Transform(
-                    //transform we use for the stable drawer  we, not need to move with scroll view
-                    transform: Matrix4.translationValues(scrollController.offset, 0.0, 0.0),
-                    child: HomeDrawer(
-                      screenIndex: widget.screenIndex == null ? DrawerIndex.HOME : widget.screenIndex,
-                      iconAnimationController: iconAnimationController,
-                      callBackIndex: (DrawerIndex indexType) {
-                        onDrawerClick();
-                        try {
-                          widget.onDrawerCall(indexType);
-                        } catch (e) {}
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                //full-screen Width with widget.screenView
-                child: Container(
-                    decoration: BoxDecoration(
-                      color: AppTheme.white,
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(color: AppTheme.darkGrey.withOpacity(0.6), blurRadius: 24),
-                      ],
-                    ),
-                    child: Stack(
-                      children: <Widget>[
-                    //this IgnorePointer we use as touch(user Interface) widget.screen View, for example scrolloffset == 1 means drawer is close we just allow touching all widget.screen View
-                    IgnorePointer(
-                    ignoring: scrolloffset == 1 || false,
-                      child: widget.screenView,
-                    ),
-                    //alternative touch(user Interface) for widget.screen, for example, drawer is close we need to tap on a few home screen area and close the drawer
-                    if (scrolloffset == 1.0)
-                InkWell(
-                onTap: () {
-          onDrawerClick();
-          },
-          ),
-          // this just menu and arrow icon animation
-          Padding(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 8, left: 8),
-            child: SizedBox(
-              width: AppBar().preferredSize.height - 8,
-              height: AppBar().preferredSize.height - 8,
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(AppBar().preferredSize.height),
-                  child: Center(
-                    // if you use your own menu view UI you add form initialization
-                    child: widget.menuView != null
-                        ? widget.menuView
-                        : AnimatedIcon(
-                        icon: widget.animatedIconData != null ? widget.animatedIconData : AnimatedIcons.arrow_menu,
-                        progress: iconAnimationController),
+                SizedBox(
+                  width: widget.drawerWidth,
+                  /* we divided first drawer Width with HomeDrawer and second
+                  full-screen Width with all home screen,
+                  we called screen View */
+                  height: MediaQuery.of(context).size.height,
+                  child: AnimatedBuilder(
+                    animation: iconAnimationController,
+                    builder: (BuildContext context, Widget child) {
+                      return Transform(
+                        /* transform we use for the stable drawer
+                        we do not need to move with scroll view */
+                        transform: Matrix4.translationValues(
+                            scrollController.offset, 0.0, 0.0),
+                        child: HomeDrawer(
+                          screenIndex: widget.screenIndex == null
+                              ? DrawerIndex.HOME
+                              : widget.screenIndex,
+                          iconAnimationController: iconAnimationController,
+                          callBackIndex: (DrawerIndex indexType) {
+                            onDrawerClick();
+                            try {
+                              widget.onDrawerCall(indexType);
+                            } catch (e) {}
+                          },
+                        ),
+                      );
+                    },
                   ),
-                  onTap: () {
-                    FocusScope.of(context).requestFocus(FocusNode());
-                    onDrawerClick();
-                  },
                 ),
-              ),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    //full-screen Width with widget.screenView
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color: AppTheme.white,
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(color: AppTheme.darkGrey.withOpacity(0.6),
+                                blurRadius: 24),
+                          ],
+                        ),
+                        child: Stack(
+                          children: <Widget>[
+                            /* this IgnorePointer we use as touch(user Interface)
+                            widget.screen View, for example scrolloffset == 1
+                            means drawer is close we just allow touching all
+                            widget.screen View */
+                            IgnorePointer(
+                              ignoring: scrolloffset == 1 || false,
+                              child: widget.screenView,
+                            ),
+                            /* alternative touch(user Interface) for
+                            widget.screen, for example, drawer is close we
+                            need to tap on a few home screen area and
+                            close the drawer */
+                             */
+                            if (scrolloffset == 1.0)
+                              InkWell(
+                                onTap: () {
+                                  onDrawerClick();
+                                },
+                              ),
+                            // this just menu and arrow icon animation
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  top: MediaQuery.of(context).padding.top + 8,
+                                  left: 8
+                              ),
+                              child: SizedBox(
+                                width: AppBar().preferredSize.height - 8,
+                                height: AppBar().preferredSize.height - 8,
+                                child: Material(
+                                  color: Colors.transparent,
+                                  child: InkWell(
+                                    borderRadius: BorderRadius.circular(AppBar()
+                                        .preferredSize.height),
+                                    child: Center(
+                                      /* if you use your own menu view UI you
+                                      add form initialization */
+                                      child: widget.menuView != null
+                                        ? widget.menuView
+                                        : AnimatedIcon(
+                                          icon: widget.animatedIconData != null
+                                              ? widget.animatedIconData
+                                              : AnimatedIcons.arrow_menu,
+                                      progress: iconAnimationController),
+                                    ),
+                                    onTap: () {
+                                      FocusScope.of(context)
+                                          .requestFocus(FocusNode());
+                                      onDrawerClick();
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                    ),
+                ),
+              ],
             ),
           ),
-          ],
         ),
       ),
-    ),
-    ],
-    ),
-    ),
-    ),
-    ),
       onWillPop: () async => false
     );
   }
 
   void onDrawerClick() {
-    //if scrollcontroller.offset != 0.0 then we set to closed the drawer(with animation to offset zero position) if is not 1 then open the drawer
+    /* if scrollcontroller.offset != 0.0 then we set to closed the
+    drawer(with animation to offset zero position) if is not 1
+    then open the drawer */
     if (scrollController.offset != 0.0) {
       scrollController.animateTo(
         0.0,
--- Code/mule/lib/config/config.dart
+++ Code/mule/lib/config/config.dart
@@ -1,4 +1,5 @@
 class Config {
-  static final BASE_URL = 'http://localhost:3000/api';
+  //static final BASE_URL = 'http://localhost:3000/api';
   //static final BASE_URL = 'http://10.0.2.2:3000/api';
+  static final BASE_URL = 'https://whisperingmule.herokuapp.com/api/';
 }
