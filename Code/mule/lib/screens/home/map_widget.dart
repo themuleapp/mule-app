465fc2e3d459a78669e03c55f5fba893b9808a41
Ji Darwish <ji.darwish98@gmail.com>
2020-06-24 00:06:02 +0200

    Removed open settings page

--- Code/mule/lib/screens/home/map_widget.dart
+++ Code/mule/lib/screens/home/map_widget.dart
@@ -64,9 +64,10 @@ class _MapWidgetState extends State<MapWidget> {
 
   @override
   Widget build(BuildContext context) {
-    if (locationIsLoaded) {
-      return getMap();
+    if (!locationIsLoaded) {
+      return Container();
     }
-    return OpenSettingsForLocation();
+    return getMap();
+    // return OpenSettingsForLocation();
   }
 }
