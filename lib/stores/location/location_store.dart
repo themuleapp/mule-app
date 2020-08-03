b62be53dbd24d108e4b382f213c074b9bf38d8c6
Mrigank Doshy <mrigankdoshy@gmail.com>
2020-08-03 15:15:16 -0400

    Destination Store

--- lib/stores/location/location_store.dart
+++ lib/stores/location/location_store.dart
@@ -1,5 +1,6 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:mobx/mobx.dart';
+import 'package:mule/models/data/suggestions.dart';
 
 part 'location_store.g.dart';
 
@@ -10,11 +11,25 @@ abstract class _LocationStore with Store {
   double lat = null;
   @observable
   double lng = null;
+  @observable
+  Suggestion destination = null;
+  @observable
+  Suggestion place = null;
 
   @action
   void updateLocation(Position newPos) {
     lat = newPos.latitude;
     lng = newPos.longitude;
+  }
+
+  @action
+  void updateDestination(Suggestion newDestination) {
+    destination = newDestination;
+  }
+
+  @action
+  void updatePlace(Suggestion newPlace) {
+    place = newPlace;
   }
 
   @computed
