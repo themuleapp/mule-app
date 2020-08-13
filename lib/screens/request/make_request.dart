aa6035d186449633857409347c833991a518a7a2
Mrigank Doshy <mrigankdoshy@gmail.com>
2020-08-13 14:58:17 -0400

    Bar UI made better

--- lib/screens/request/make_request.dart
+++ lib/screens/request/make_request.dart
@@ -94,71 +94,70 @@ class _MakeRequestScreenState extends State<MakeRequestScreen>
                             child: Padding(
                               padding: const EdgeInsets.only(
                                   left: 16, right: 16, top: 8, bottom: 8),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: AppTheme.white,
-                                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: AppTheme.lightGrey.withOpacity(0.1),
-                                          spreadRadius: 5,
-                                          blurRadius: 7,
-                                          offset: Offset(0, 3), // changes position of shadow
-                                        ),
-                                      ],
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: AppTheme.white,
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(10)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: AppTheme.lightGrey
+                                          .withOpacity(0.1),
+                                      spreadRadius: 5,
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3), // changes position of shadow
                                     ),
-                                    child: TextField(
-                                      decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        contentPadding: EdgeInsets.only(top: 15),
-                                        hintText: "From here - address",
-                                        prefixIcon: IconButton(
-                                          splashColor: AppTheme.lightBlue,
-                                          icon: Icon(
-                                            Icons.my_location,
-                                            color: AppTheme.secondaryBlue,
+                                  ],
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.only(left: 10, right: 10),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Container(
+                                        child: TextField(
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            contentPadding: EdgeInsets.only(
+                                                top: 15),
+                                            hintText: "From here - address",
+                                            prefixIcon: IconButton(
+                                              splashColor: AppTheme.lightBlue,
+                                              icon: Icon(
+                                                Icons.my_location,
+                                                color: AppTheme.secondaryBlue,
+                                              ),
+                                              onPressed: () {},
+                                            ),
                                           ),
-                                          onPressed: () {},
                                         ),
                                       ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10
-                                  ),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: AppTheme.white,
-                                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: AppTheme.lightGrey.withOpacity(0.1),
-                                          spreadRadius: 5,
-                                          blurRadius: 7,
-                                          offset: Offset(0, 3), // changes position of shadow
-                                        ),
-                                      ],
-                                    ),
-                                    child: TextField(
-                                      decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        contentPadding: EdgeInsets.only(top: 15),
-                                        hintText: "To here - address",
-                                        prefixIcon: IconButton(
-                                          splashColor: AppTheme.lightBlue,
-                                          icon: Icon(
-                                            Icons.place,
-                                            color: AppTheme.secondaryBlue,
-                                          ),
-                                          onPressed: () {},
-                                        ),
+                                      SizedBox (
+                                        height: 10
                                       ),
-                                    ),
+                                      Container(
+                                        child: TextField(
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            contentPadding: EdgeInsets.only(
+                                                top: 15),
+                                            hintText: "To here - address",
+                                            prefixIcon: IconButton(
+                                              splashColor: AppTheme.lightBlue,
+                                              icon: Icon(
+                                                Icons.place,
+                                                color: AppTheme.secondaryBlue,
+                                              ),
+                                              onPressed: () {},
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
                                   )
-                                ],
-                              ),
+                                )
+                              )
                             ),
                           ),
                         ),
@@ -188,7 +187,8 @@ class _MakeRequestScreenState extends State<MakeRequestScreen>
                                       ),
                                       child: Icon(
                                         Icons.close,
-                                        color: AppTheme.lightGrey.withOpacity(0.5),
+                                        color: AppTheme.lightGrey
+                                            .withOpacity(0.5),
                                         size: 28,
                                       ),
                                     ),
