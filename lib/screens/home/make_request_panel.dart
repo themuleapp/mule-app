06622a8c8aa2051098a0fbd41834dce164b5ad87
Zohar Cochavi <zohar.cochavi@gmail.com>
2020-08-15 19:06:04 +0200

    Adds states and a new panel to the slider panel

---
+++ lib/screens/home/make_request_panel.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:mule/config/app_theme.dart';
+
+class MakeRequestPanel extends StatefulWidget {
+  _MakeRequestPanelState createState() => _MakeRequestPanelState();
+}
+
+class _MakeRequestPanelState extends State {
+  double opacity = 1.0;
+
+  @override
+  build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: opacity,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 32.0, left: 18, right: 16),
+            child: Text(
+              'Confirm Details',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 22,
+                letterSpacing: 0.27,
+                color: AppTheme.darkerText,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, bottom: 8, top: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.people,
+                      color: AppTheme.secondaryBlue,
+                      size: 24,
+                    ),
+                    Text(
+                      ' 12 ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 21,
+                        color: AppTheme.lightGrey,
+                      ),
+                    ),
+                    Text(
+                      'Mules around',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w200,
+                        fontSize: 21,
+                        color: AppTheme.lightGrey,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: opacity,
+          child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: Container(
+                  decoration: BoxDecoration(
+                    color: AppTheme.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppTheme.lightGrey.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(top: 15),
+                                hintText: "From here - address",
+                                prefixIcon: IconButton(
+                                  splashColor: AppTheme.lightBlue,
+                                  icon: Icon(
+                                    Icons.my_location,
+                                    color: AppTheme.secondaryBlue,
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Container(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(top: 15),
+                                hintText: "To here - address",
+                                prefixIcon: IconButton(
+                                  splashColor: AppTheme.lightBlue,
+                                  icon: Icon(
+                                    Icons.place,
+                                    color: AppTheme.secondaryBlue,
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )))),
+        ),
+        SizedBox(height: 10),
+        AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: opacity,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, bottom: 8, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                GestureDetector(
+                  child: Container(
+                    width: 48,
+                    height: 48,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppTheme.white,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(16.0),
+                        ),
+                        border: Border.all(
+                            color: AppTheme.lightGrey.withOpacity(0.2)),
+                      ),
+                      child: Icon(
+                        Icons.close,
+                        color: AppTheme.lightGrey.withOpacity(0.5),
+                        size: 28,
+                      ),
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                    child: GestureDetector(
+                  child: Container(
+                    height: 48,
+                    decoration: BoxDecoration(
+                      color: AppTheme.secondaryBlue,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(16.0),
+                      ),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                            color: AppTheme.secondaryBlue.withOpacity(0.5),
+                            offset: const Offset(1.1, 1.1),
+                            blurRadius: 10.0),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Request',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          letterSpacing: 0.0,
+                          color: AppTheme.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  onTap: () {},
+                ))
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).padding.bottom,
+        )
+      ],
+    );
+  }
+}
--- lib/screens/home/sliding_up_widget.dart
+++ lib/screens/home/sliding_up_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mule/screens/home/make_request_panel.dart';
 import 'package:mule/screens/home/map_widget.dart';
 import 'package:mule/screens/home/slider_form_widget.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -9,15 +10,15 @@ class SlidingUpWidget extends StatefulWidget {
 }
 
 class _SlidingUpWidgetState extends State<SlidingUpWidget> {
-  // final FocusNode _fakeFocusNode = FocusNode();
   final FocusNode _destinationFocusNode = FocusNode();
   final PanelController _panelController = PanelController();
-  final BorderRadiusGeometry radius = BorderRadius.only(
-    topLeft: Radius.circular(20.0),
-    topRight: Radius.circular(20.0),
-  );
+  final double radius = 20.0;
+
   bool panelIsOpen = false;
   bool programmaticallyOpeningOrClosing = false;
+
+  PanelIndex panelIndex;
+  Widget _currentPanel;
 
   _openPanel() {
     _panelController.open().then((value) => setState(() {
@@ -57,26 +58,52 @@ class _SlidingUpWidgetState extends State<SlidingUpWidget> {
   @override
   void initState() {
     _destinationFocusNode.addListener(_handleSearchFocus);
+    setState(() => panelIndex = PanelIndex.DestinationAndSearch);
+
+    _updatePanel();
     super.initState();
+  }
+
+  void _updatePanel() {
+    switch (panelIndex) {
+      case PanelIndex.DestinationAndSearch:
+        _setCurrentPanel(SliderFormWidget(
+          panelIsOpen: panelIsOpen,
+          destinationFocusNode: _destinationFocusNode,
+        ));
+        break;
+      case PanelIndex.MakeRequest:
+        _setCurrentPanel(MakeRequestPanel());
+        break;
+      default:
+        throw UnimplementedError("Called panel not implemented");
+    }
+  }
+
+  void _setCurrentPanel(panel) {
+    setState(() => _currentPanel = panel);
   }
 
   Widget _panel(ScrollController sc) {
     return SingleChildScrollView(
-        physics: ClampingScrollPhysics(),
-        controller: sc,
-        child: SliderFormWidget(
-          panelIsOpen: panelIsOpen,
-          destinationFocusNode: _destinationFocusNode,
-        ));
+      physics: ClampingScrollPhysics(),
+      controller: sc,
+      child: _currentPanel,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     return SlidingUpPanel(
-      borderRadius: radius,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(radius),
+        topRight: Radius.circular(radius),
+      ),
       onPanelSlide: this._handlePanelSlide,
+      isDraggable: false,
       minHeight: screenHeight / 4,
+      snapPoint: 1 / 4,
       maxHeight: screenHeight - 120,
       controller: _panelController,
       backdropEnabled: true,
@@ -86,4 +113,9 @@ class _SlidingUpWidgetState extends State<SlidingUpWidget> {
       ),
     );
   }
+}
+
+enum PanelIndex {
+  DestinationAndSearch,
+  MakeRequest,
 }
