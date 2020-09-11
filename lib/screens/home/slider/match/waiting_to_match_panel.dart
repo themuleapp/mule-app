6c7438125acaa4d2d27de1d2852450520b152e4c
Zohar Cochavi <zohar.cochavi@gmail.com>
2020-09-11 13:53:08 +0200

    Fix resizing issue with loading bar

--- lib/screens/home/slider/match/waiting_to_match_panel.dart
+++ lib/screens/home/slider/match/waiting_to_match_panel.dart
@@ -8,7 +8,7 @@ class WaitingToMatchPanel extends StatelessWidget {
   final double opacity = 1.0;
 
   WaitingToMatchPanel(
-      {this.slidingUpWidgetController, this.loadingBarHeight = 10.0});
+      {this.slidingUpWidgetController, this.loadingBarHeight = 5.0});
 
   @override
   build(BuildContext context) {
@@ -19,55 +19,74 @@ class WaitingToMatchPanel extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Center(
-          child: Container(
-            height: loadingBar,
-            child: ClipRect(
-              clipper: ClipHeightQuarter(height: loadingBarHeight),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(loadingBar / 2)),
-                child: LinearProgressIndicator(
-                  minHeight: loadingBarHeight,
-                  backgroundColor: AppTheme.secondaryBlue,
-                  valueColor: AlwaysStoppedAnimation<Color>(AppTheme.lightBlue),
+        Stack(
+          children: <Widget>[
+            Center(
+              child: Container(
+                height: loadingBar,
+                child: ClipRect(
+                  clipper: ClipHeightQuarter(height: loadingBarHeight),
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(loadingBar / 2)),
+                    child: LinearProgressIndicator(
+                      minHeight: loadingBarHeight,
+                      backgroundColor: AppTheme.secondaryBlue,
+                      valueColor:
+                          AlwaysStoppedAnimation<Color>(AppTheme.lightBlue),
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        AnimatedOpacity(
-          duration: const Duration(milliseconds: 500),
-          opacity: opacity,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: Text(
-              'Your request has been sent!',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 22,
-                color: AppTheme.darkerText,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      AnimatedOpacity(
+                        duration: const Duration(milliseconds: 500),
+                        opacity: opacity,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          child: Text(
+                            'Your request has been sent!',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 22,
+                              color: AppTheme.darkerText,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 16, left: 16, right: 16, bottom: 8),
+                        child: Text(
+                            "Hang tight! We will notify you as soon as "
+                            "someone accepts your request",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: AppTheme.elementSize(screenHeight, 14,
+                                    15, 16, 17, 18, 20, 24, 28),
+                                fontWeight: FontWeight.w500,
+                                color: AppTheme.darkGrey)),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.only(top: 16, right: 16),
+                    child: Image.asset('assets/images/student_waiting.png',
+                        height: 150))
+              ],
             ),
-          ),
+          ],
         ),
-        Padding(
-          padding:
-              const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 8),
-          child: Text(
-              "Hang tight! We will notify you as soon as "
-              "someone accepts your request",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: AppTheme.elementSize(
-                      screenHeight, 14, 15, 16, 17, 18, 20, 24, 28),
-                  fontWeight: FontWeight.w500,
-                  color: AppTheme.darkGrey)),
-        ),
-        Container(
-            padding: const EdgeInsets.only(top: 16, right: 16),
-            child:
-                Image.asset('assets/images/student_waiting.png', height: 150))
       ],
     );
   }
