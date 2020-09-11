c990ee86e343b3fe7c35e0a341671a66b6bae04c
Ji Darwish <j.darwish@gerimedica.nl>
2020-09-11 12:40:20 +0200

    Confirm accept or reject

---
+++ lib/widgets/confirm_dialogue.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+createConfirmDialogue(context, actionType) async {
+  // flutter defined function
+  return await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      // return object of type Dialog
+      return AlertDialog(
+        content: Text("Are you sure you want to $actionType this request?"),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Ok"),
+            onPressed: () {
+              // Dismiss the dialog and
+              // also dismiss the swiped item
+              print('dismissed');
+              Navigator.pop(context, true);
+            },
+          ),
+          FlatButton(
+            child: Text('Cancel'),
+            onPressed: () {
+              // Dismiss the dialog but don't
+              // dismiss the swiped item
+              print('reject');
+              Navigator.pop(context, false);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
