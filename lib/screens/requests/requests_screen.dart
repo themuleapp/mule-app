e1321821ba2fb0239cf30bd2e334dbbfd25839e5
Mrigank Doshy <mrigankdoshy@gmail.com>
2020-09-12 12:18:05 -0400

    Refactor

--- lib/screens/requests/requests_screen.dart
+++ lib/screens/requests/requests_screen.dart
@@ -4,6 +4,7 @@ import 'package:mule/config/http_client.dart';
 import 'package:mule/models/res/requestedFromMeRes/requested_from_me_res.dart';
 import 'package:mule/widgets/alert_widget.dart';
 import 'package:mule/widgets/confirm_dialogue.dart';
+import 'package:mule/widgets/tab.dart';
 
 class RequestsScreen extends StatefulWidget {
   static final String ACCEPT = 'accept';
@@ -162,58 +163,26 @@ class _RequestsScreenState extends State<RequestsScreen>
       children: <Widget> [
         TabBar(
           tabs: [
-            Container(
-              height: 30,
-              child: Text(
-                'Available',
-                style: TextStyle(
-                  fontFamily: AppTheme.fontName,
-                  fontWeight: FontWeight.w500,
-                  fontSize: AppTheme.elementSize(screenHeight,
-                      14, 15, 16, 17, 18, 20, 24, 28),
-                ),
-              ),
-            ),
-            Container(
-              height: 30,
-              child: Text(
-                'Dismissed',
-                style: TextStyle(
-                  fontFamily: AppTheme.fontName,
-                  fontWeight: FontWeight.w500,
-                  fontSize: AppTheme.elementSize(screenHeight,
-                      14, 15, 16, 17, 18, 20, 24, 28),
-                ),
-              ),
-            ),
-            Container(
-              height: 30,
-              child: Text(
-                'Ongoing',
-                style: TextStyle(
-                  fontFamily: AppTheme.fontName,
-                  fontWeight: FontWeight.w500,
-                  fontSize: AppTheme.elementSize(screenHeight,
-                      14, 15, 16, 17, 18, 20, 24, 28),
-                ),
-              ),
-            )
+            tab('Available', screenHeight),
+            tab('Dismissed', screenHeight),
+            tab('Ongoing', screenHeight),
           ],
           unselectedLabelColor: AppTheme.lightestGrey,
           indicatorColor: AppTheme.secondaryBlue,
           labelColor: AppTheme.black,
           indicatorSize: TabBarIndicatorSize.tab,
-          indicatorWeight: 3.0,
+          indicatorWeight: 4.0,
           isScrollable: false,
           controller: _tabController,
         ),
         Container(
+          padding: EdgeInsets.only(top: 16, right: 16, left: 16),
           height: 100,
           child: TabBarView(
               controller: _tabController,
               children: <Widget>[
                 Container(
-                    child: generateItemsList()
+                  child: generateItemsList()
                 ),
                 Container(
                   child: Text("Dismissed"),
