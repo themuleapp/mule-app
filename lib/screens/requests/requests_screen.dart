f77f2ef26300395c12c016731dbedf373e01fb77
Mrigank Doshy <mrigankdoshy@gmail.com>
2020-09-22 19:26:15 -0400

    Added card to request screen (incomplete)

--- lib/screens/requests/requests_screen.dart
+++ lib/screens/requests/requests_screen.dart
@@ -46,9 +46,74 @@ class _RequestsScreenState extends State<RequestsScreen>
               onTap: () {
                 print("${requestedFromMe[index].requestedItem} clicked");
               },
-              child: ListTile(
-                  title: Text('${requestedFromMe[index].requestedItem}')
-              )
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 500),
+              opacity: 1.0,
+              child: Padding(
+                padding:
+                const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: AppTheme.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppTheme.lightGrey.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(top: 15),
+                              hintText: requestedFromMe[index].place,
+                              prefixIcon: IconButton(
+                                splashColor: AppTheme.lightBlue,
+                                icon: Icon(
+                                  Icons.my_location,
+                                  color: AppTheme.secondaryBlue,
+                                ),
+                                onPressed: () {},
+                              ),
+                              enabled: false,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(top: 15),
+                              hintText: requestedFromMe[index].destination,
+                              prefixIcon: IconButton(
+                                splashColor: AppTheme.lightBlue,
+                                icon: Icon(
+                                  Icons.place,
+                                  color: AppTheme.secondaryBlue,
+                                ),
+                                onPressed: () {},
+                              ),
+                              enabled: false,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
           background: slideRightBackground(),
           secondaryBackground: slideLeftBackground(),
@@ -177,13 +242,11 @@ class _RequestsScreenState extends State<RequestsScreen>
         ),
         Container(
           padding: EdgeInsets.only(top: 16, right: 16, left: 16),
-          height: 100,
+          height: screenHeight,
           child: TabBarView(
               controller: _tabController,
               children: <Widget>[
-                Container(
-                  child: generateItemsList()
-                ),
+                generateItemsList(),
                 Container(
                   child: Text("Dismissed"),
                 ),
