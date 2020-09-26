44f08a4d969ecf9efc469880df3105716ba7ba61
Zohar Cochavi <zohar.cochavi@gmail.com>
2020-09-26 10:22:12 +0200

    Loads current location together with splash screen when entering the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:mule/config/config.dart';
-import 'package:mule/config/http_client.dart';
-import 'package:mule/models/res/profileRes/profile_res.dart';
 import 'package:mule/navigation_home_screen.dart';
 import 'package:mule/screens/welcome_screen.dart';
 import 'package:mule/splash_screen.dart';
-import 'package:mule/stores/global/user_info_store.dart';
+import 'package:mule/config/utils.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,28 +12,17 @@ void main() async {
 }
 
 class App extends StatelessWidget {
-  final Future<bool> _isAuthenticatedUser =
-      Config.getToken().then((value) async {
-    if (value != null && value.isNotEmpty) {
-      final Response res = await httpClient.handleGetProfileData();
-      if (res.statusCode == 200) {
-        ProfileRes profileRes = ProfileRes.fromJson(res.data);
-        GetIt.I.get<UserInfoStore>().updateEverythingFromrRes(profileRes);
-        GetIt.I.get<UserInfoStore>().updateProfilePicture();
-        return true;
-      }
-    }
-    return false;
-  });
+  final Future<bool> _isAuthenticatedUser = authenticateUser();
+  final Future<bool> _isCurrentLocationLoaded = loadCurrentPosition();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: FutureBuilder(
-        future: _isAuthenticatedUser,
-        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          if (snapshot.hasData && snapshot.data) {
+        future: Future.wait([_isAuthenticatedUser, _isCurrentLocationLoaded]),
+        builder: (BuildContext context, AsyncSnapshot<List<bool>> snapshot) {
+          if (snapshot.hasData && !snapshot.data.contains(false)) {
             return NavigationHomeScreen();
           } else if (!snapshot.hasData) {
             return SplashScreen();
