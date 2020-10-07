d23b28c67cbe2f3770d42d1324cb3159bfe5056f
Zohar Cochavi <zohar.cochavi@gmail.com>
2020-10-07 11:10:13 +0200

    Fixes locationstore not updating when logging in with active request

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mule/config/http_client.dart';
 import 'package:mule/models/data/order_data.dart';
+import 'package:mule/models/data/suggestion.dart';
 import 'package:mule/screens/home/slider/sliding_up_widget.dart';
 import 'package:mule/stores/global/user_info_store.dart';
+import 'package:mule/stores/location/location_store.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key key}) : super(key: key);
@@ -46,28 +48,36 @@ class _MyHomePageState extends State<MyHomePage>
                   maxHeight: screenHeight - 120,
                   controller: controller,
                 );
-                if (snapshot.hasData && snapshot.data != null) {
-                  // TODO Replace fullname check by user being a mule or not
-                  if (snapshot.data.createdBy !=
-                      GetIt.I.get<UserInfoStore>().fullName) {
-                    controller.panelIndex = PanelIndex.DestinationAndSearch;
-                  } else {
-                    switch (snapshot.data.status) {
-                      case (Status.ACCEPTED):
-                        controller.panelIndex = PanelIndex.Matched;
-                        break;
-                      case (Status.OPEN):
-                        controller.panelIndex = PanelIndex.WaitingToMatch;
-                        break;
-                      default:
-                        controller.panelIndex = PanelIndex.DestinationAndSearch;
-                    }
-                  }
-                }
+                _checkCurrentActiveStatus(snapshot);
                 return slidingUpWidget;
               }),
         ),
       ),
     );
+  }
+
+  _checkCurrentActiveStatus(snapshot) {
+    if (snapshot.hasData && snapshot.data != null) {
+      // TODO Replace fullname check by user being a mule or not
+      if (snapshot.data.createdBy != GetIt.I.get<UserInfoStore>().fullName) {
+        controller.panelIndex = PanelIndex.DestinationAndSearch;
+      } else {
+        switch (snapshot.data.status) {
+          case (Status.ACCEPTED):
+            controller.panelIndex = PanelIndex.Matched;
+            break;
+          case (Status.OPEN):
+            controller.panelIndex = PanelIndex.WaitingToMatch;
+            break;
+          default:
+            controller.panelIndex = PanelIndex.DestinationAndSearch;
+        }
+        OrderData order = snapshot.data;
+        GetIt.I.get<LocationStore>().updateDestination(DestinationSuggestion(
+            "", order.destination.description, order.destination.location));
+        GetIt.I.get<LocationStore>().updatePlace(PlacesSuggestion(
+            "", order.place.description, order.place.location));
+      }
+    }
   }
 }
