a0d2369fec2b703085b336afde41116d5670ed43
Zohar Cochavi <zohar.cochavi@gmail.com>
2021-01-16 15:55:53 +0100

    Fancy OOP inheritance

--- lib/models/data/user_data.dart
+++ lib/models/data/user_data.dart
@@ -1,10 +1,26 @@
+import 'package:mule/models/data/location_data.dart';
+
 class UserData {
-  final String name;
-  final String phoneNumber;
+  String name;
+  String phoneNumber;
 
   UserData({this.name, this.phoneNumber});
 
   UserData.fromJson(Map<String, dynamic> jsonData)
       : this.name = jsonData['name'],
         this.phoneNumber = jsonData['phoneNumber'];
+}
+
+class MuleData extends UserData {
+  String name;
+  String phoneNumber;
+  LocationData location;
+
+  MuleData({String name, String phoneNumber, this.location})
+      : super(name: name, phoneNumber: phoneNumber);
+
+  MuleData.fromJson(Map<String, dynamic> jsonData)
+      : name = jsonData['name'],
+        phoneNumber = jsonData['phoneNumber'],
+        this.location = LocationData.fromJson(jsonData['location']);
 }
