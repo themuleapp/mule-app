67f019fd50bd05ef393a118a6078ffabdeaffc3d
Mrigank Doshy <mrigankdoshy@gmail.com>
2021-01-15 20:07:01 -0500

    Accepted By phone number

--- lib/models/data/mule_data.dart
+++ lib/models/data/mule_data.dart
@@ -3,10 +3,12 @@ import 'package:mule/models/data/location_data.dart';
 class MuleData {
   final String name;
   final LocationData location;
+  final String phoneNumber;
 
-  MuleData({this.name, this.location});
+  MuleData({this.name, this.location, this.phoneNumber});
 
   MuleData.fromJson(Map<String, dynamic> jsonData)
       : this.name = jsonData['name'],
+        this.phoneNumber = jsonData['phoneNumber'],
         this.location = LocationData.fromJson(jsonData['location']);
 }
