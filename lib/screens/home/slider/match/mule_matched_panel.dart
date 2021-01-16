f431653e636fc3861663de46d4576a37e9d22966
Zohar Cochavi <zohar.cochavi@gmail.com>
2021-01-16 17:18:05 +0100

    FutureBuilder for MuleMatchedPanel

--- lib/screens/home/slider/match/mule_matched_panel.dart
+++ lib/screens/home/slider/match/mule_matched_panel.dart
@@ -30,35 +30,30 @@ class MuleMatchedPanel extends StatefulWidget {
 }
 
 class _MuleMatchedPanelState extends State<MuleMatchedPanel> {
-  OrderData order;
-
   final MessagesService _service = GetIt.I.get<MessagesService>();
-  String number = "";
+
+  Future<OrderData> order = httpClient.getActiveRequest();
 
   @override
   void initState() {
     super.initState();
     widget.buttonBridge?.callback = cancelRequest;
     widget.buttonBridge2?.callback = completedRequest;
-    number = order.createdBy.phoneNumber;
   }
 
-  Future<OrderData> updateOrder() async {
-    setState(() async {
-      order = await httpClient.getActiveRequest();
-      if (order.status == Status.ACCEPTED) {
-        widget.mapController.updateDelivery(
-          order.acceptedBy.location.toLatLng(),
-          order.place.location.toLatLng(),
-          order.destination.location.toLatLng(),
-          .1,
-        );
-      }
-    });
+  updateOrder(OrderData order) {
+    if (order.status == Status.ACCEPTED) {
+      widget.mapController.updateDelivery(
+        order.acceptedBy.location.toLatLng(),
+        order.place.location.toLatLng(),
+        order.destination.location.toLatLng(),
+        .1,
+      );
+    }
   }
 
   cancelRequest() async {
-    if (await httpClient.deleteActiveRequest(order)) {
+    if (await httpClient.deleteActiveRequest(await order)) {
       widget.slidingUpWidgetController.panelIndex =
           PanelIndex.DestinationAndSearch;
       widget.mapController.focusCurrentLocation();
@@ -75,109 +70,126 @@ class _MuleMatchedPanelState extends State<MuleMatchedPanel> {
   @override
   build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
-    updateOrder();
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        AnimatedOpacity(
-          duration: const Duration(milliseconds: 500),
-          opacity: widget.opacity,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 32.0, left: 16, right: 16),
-            child: Text(
-              'You are en route',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 22,
-                color: AppTheme.darkerText,
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                height: 60,
-                width: 60,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                  //color: Colors.white,
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: Image.asset(
-                      'assets/images/profile_picture_placeholder.png'),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.only(left: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Text(
-                        (order != null) ? order.createdBy.name : "LOADING",
-                        style: TextStyle(
-                          color: AppTheme.darkerText,
-                          fontWeight: FontWeight.w700,
-                          fontSize: AppTheme.elementSize(
-                              screenHeight, 16, 16, 17, 17, 18, 24, 26, 28),
-                        ),
+    return FutureBuilder(
+        future: order,
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return CircularProgressIndicator();
+          } else {
+            // updateOrder(snapshot.data);
+            UserData user = snapshot.data.requestedBy;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AnimatedOpacity(
+                  duration: const Duration(milliseconds: 500),
+                  opacity: widget.opacity,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 32.0, left: 16, right: 16),
+                    child: Text(
+                      'You are en route',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 22,
+                        color: AppTheme.darkerText,
                       ),
-                      SizedBox(height: 5),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Icon(
-                            Icons.perm_phone_msg,
-                            color: AppTheme.secondaryBlue,
-                            size: AppTheme.elementSize(
-                                screenHeight, 25, 25, 26, 26, 18, 20, 21, 22),
-                          ),
-                          Text(
-                              order.createdBy
-                                  .phoneNumber, //replace with phone number
-                              style: TextStyle(
-                                color: AppTheme.lightGrey,
-                                fontWeight: FontWeight.w500,
-                                fontSize: AppTheme.elementSize(screenHeight, 14,
-                                    14, 15, 15, 16, 18, 21, 24),
-                              )),
-                        ],
-                      )
-                    ],
+                    ),
                   ),
                 ),
-              ),
-              GestureDetector(
-                child: Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                      color: AppTheme.lightGrey.withOpacity(0.1),
-                    ),
-                    child: Icon(
-                      Icons.chat,
-                      color: AppTheme.secondaryBlue,
-                      size: AppTheme.elementSize(
-                          screenHeight, 25, 25, 26, 26, 28, 36, 38, 40),
-                    )),
-                onTap: () => _service.sendSms(number),
-              ),
-              //SizedBox(width: 15),
-            ],
-          ),
-        )
-      ],
-    );
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                          //color: Colors.white,
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(16.0),
+                          child: Image.asset(
+                              'assets/images/profile_picture_placeholder.png'),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Text(
+                                user.name,
+                                style: TextStyle(
+                                  color: AppTheme.darkerText,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: AppTheme.elementSize(screenHeight,
+                                      16, 16, 17, 17, 18, 24, 26, 28),
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.perm_phone_msg,
+                                    color: AppTheme.secondaryBlue,
+                                    size: AppTheme.elementSize(screenHeight, 25,
+                                        25, 26, 26, 18, 20, 21, 22),
+                                  ),
+                                  Text(
+                                      user.phoneNumber, //replace with phone number
+                                      style: TextStyle(
+                                        color: AppTheme.lightGrey,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: AppTheme.elementSize(
+                                            screenHeight,
+                                            14,
+                                            14,
+                                            15,
+                                            15,
+                                            16,
+                                            18,
+                                            21,
+                                            24),
+                                      )),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        child: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8.0)),
+                              color: AppTheme.lightGrey.withOpacity(0.1),
+                            ),
+                            child: Icon(
+                              Icons.chat,
+                              color: AppTheme.secondaryBlue,
+                              size: AppTheme.elementSize(
+                                  screenHeight, 25, 25, 26, 26, 28, 36, 38, 40),
+                            )),
+                        onTap: () => _service.sendSms(user.phoneNumber),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            );
+          }
+        });
   }
 }
