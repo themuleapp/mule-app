9c7761c9d7ad564e738c55775215442e3fad7716
Zohar Cochavi <zohar.cochavi@gmail.com>
2021-02-11 22:19:06 +0100

    Load current order into UserInfoStore on login

--- lib/screens/home/slider/match/user_matched_panel.dart
+++ lib/screens/home/slider/match/user_matched_panel.dart
@@ -7,8 +7,8 @@ import 'package:mule/models/data/user_data.dart';
 import 'package:mule/models/data/order_data.dart';
 import 'package:mule/screens/home/map/map_widget.dart';
 import 'package:mule/screens/home/slider/sliding_up_widget.dart';
+import 'package:mule/stores/global/user_info_store.dart';
 import 'package:mule/widgets/alert_widget.dart';
-import 'package:mule/widgets/clip_height.dart';
 import 'package:mule/widgets/stylized_button.dart';
 
 class UserMatchedPanel extends StatefulWidget {
@@ -30,7 +30,7 @@ class UserMatchedPanel extends StatefulWidget {
 }
 
 class _UserMatchedPanelState extends State<UserMatchedPanel> {
-  Future<OrderData> order = muleApiService.getActiveRequest();
+  OrderData order = GetIt.I.get<UserInfoStore>().activeOrder;
 
   final MessagesService _service = GetIt.I.get<MessagesService>();
 
@@ -54,7 +54,6 @@ class _UserMatchedPanelState extends State<UserMatchedPanel> {
   }
 
   cancelRequest() async {
-    OrderData order = await this.order;
     if (await muleApiService.userDeleteActiveRequest(order)) {
       widget.slidingUpWidgetController.panelIndex =
           PanelIndex.DestinationAndSearch;
@@ -64,166 +63,145 @@ class _UserMatchedPanelState extends State<UserMatchedPanel> {
     }
   }
 
+  Image getMuleImage(MuleData mule) {
+    if (mule.profilePicture != null) {
+      return Image.network(mule.profilePicture);
+    } else {
+      return Image.asset('assets/images/profile_picture_placeholder.png');
+    }
+  }
+
+  String getFormattedPhoneNumber(MuleData mule) {
+    return mule.phoneNumber.substring(0, 2) +
+        " " +
+        "(" +
+        mule.phoneNumber.substring(2, 5) +
+        ") " +
+        mule.phoneNumber.substring(5, 8) +
+        " - " +
+        mule.phoneNumber.substring(8, mule.phoneNumber.length);
+  }
+
   @override
   build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
-    final double loadingBar = widget.slidingUpWidgetController.radius * 2;
-    return FutureBuilder(
-        future: order,
-        builder: (context, snapshot) {
-          if (snapshot.data == null) {
-            return Center(
-              child: Container(
-                height: loadingBar,
-                child: ClipRect(
-                  clipper: ClipHeightQuarter(height: widget.loadingBarHeight),
-                  child: ClipRRect(
-                    borderRadius:
-                        BorderRadius.all(Radius.circular(loadingBar / 2)),
-                    child: LinearProgressIndicator(
-                      minHeight: widget.loadingBarHeight,
-                      backgroundColor: AppTheme.secondaryBlue,
-                      valueColor:
-                          AlwaysStoppedAnimation<Color>(AppTheme.lightBlue),
-                    ),
-                  ),
+
+    updateOrder(order);
+    MuleData mule = order.acceptedBy;
+    Image muleImg = getMuleImage(mule);
+    String phoneNumber = getFormattedPhoneNumber(mule);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: widget.opacity,
+          child: Padding(
+            padding: EdgeInsets.only(
+                top: AppTheme.elementSize(
+                    screenHeight, 20, 22, 24, 26, 0, 0, 0, 0),
+                left: 16,
+                right: 16),
+            child: Text(
+              'En Route',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: AppTheme.elementSize(
+                    screenHeight, 18, 19, 19, 20, 22, 26, 30, 36),
+                color: AppTheme.darkerText,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  //color: Colors.white,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: muleImg,
                 ),
               ),
-            );
-          } else {
-            updateOrder(snapshot.data);
-            MuleData mule = snapshot.data.acceptedBy;
-            Image muleImg;
-            if (mule.profilePicture != null) {
-              muleImg = Image.network(mule.profilePicture);
-            } else {
-              muleImg =
-                  Image.asset('assets/images/profile_picture_placeholder.png');
-            }
-
-            String formattedPhoneNumber = mule.phoneNumber.substring(0, 2) +
-                " " +
-                "(" +
-                mule.phoneNumber.substring(2, 5) +
-                ") " +
-                mule.phoneNumber.substring(5, 8) +
-                " - " +
-                mule.phoneNumber.substring(8, mule.phoneNumber.length);
-            return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  AnimatedOpacity(
-                    duration: const Duration(milliseconds: 500),
-                    opacity: widget.opacity,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          top: AppTheme.elementSize(
-                              screenHeight, 20, 22, 24, 26, 0, 0, 0, 0),
-                          left: 16,
-                          right: 16),
-                      child: Text(
-                        'En Route',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: AppTheme.elementSize(
-                              screenHeight, 18, 19, 19, 20, 22, 26, 30, 36),
-                          color: AppTheme.darkerText,
-                        ),
+              Expanded(
+                  child: Container(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      (mule != null) ? mule.name : "LOADING",
+                      style: TextStyle(
+                        color: AppTheme.darkerText,
+                        fontWeight: FontWeight.w700,
+                        fontSize: AppTheme.elementSize(
+                            screenHeight, 16, 16, 17, 17, 18, 24, 26, 28),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 16, left: 16, right: 16),
-                    child: Row(
+                    SizedBox(height: 5),
+                    Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
-                        Container(
-                          height: 60,
-                          width: 60,
-                          decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(16.0)),
-                            //color: Colors.white,
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(16.0),
-                            child: muleImg,
-                          ),
+                        Icon(
+                          Icons.phone,
+                          color: AppTheme.darkGrey,
+                          size: AppTheme.elementSize(
+                              screenHeight, 25, 25, 26, 26, 18, 20, 21, 22),
                         ),
-                        Expanded(
-                            child: Container(
-                          padding: const EdgeInsets.only(left: 16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Text(
-                                (mule != null) ? mule.name : "LOADING",
-                                style: TextStyle(
-                                  color: AppTheme.darkerText,
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: AppTheme.elementSize(screenHeight,
-                                      16, 16, 17, 17, 18, 24, 26, 28),
-                                ),
+                        Text(phoneNumber,
+                            style: TextStyle(
+                              color: AppTheme.lightGrey,
+                              fontWeight: FontWeight.w500,
+                              fontSize: AppTheme.elementSize(
+                                screenHeight,
+                                14,
+                                14,
+                                15,
+                                15,
+                                16,
+                                18,
+                                21,
+                                24,
                               ),
-                              SizedBox(height: 5),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.phone,
-                                    color: AppTheme.darkGrey,
-                                    size: AppTheme.elementSize(screenHeight, 25,
-                                        25, 26, 26, 18, 20, 21, 22),
-                                  ),
-                                  Text(formattedPhoneNumber,
-                                      style: TextStyle(
-                                        color: AppTheme.lightGrey,
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: AppTheme.elementSize(
-                                          screenHeight,
-                                          14,
-                                          14,
-                                          15,
-                                          15,
-                                          16,
-                                          18,
-                                          21,
-                                          24,
-                                        ),
-                                      )),
-                                ],
-                              )
-                            ],
-                          ),
-                        )),
-                        GestureDetector(
-                          child: Container(
-                              height: 50,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8.0)),
-                                color: AppTheme.lightGrey.withOpacity(0.1),
-                              ),
-                              child: Icon(
-                                Icons.chat,
-                                color: AppTheme.secondaryBlue,
-                                size: AppTheme.elementSize(screenHeight, 25, 25,
-                                    26, 26, 28, 36, 38, 40),
-                              )),
-                          onTap: () => _service.sendSms(mule.phoneNumber),
-                        ),
+                            )),
                       ],
+                    )
+                  ],
+                ),
+              )),
+              GestureDetector(
+                child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      color: AppTheme.lightGrey.withOpacity(0.1),
                     ),
-                  )
-                ]);
-          }
-        });
+                    child: Icon(
+                      Icons.chat,
+                      color: AppTheme.secondaryBlue,
+                      size: AppTheme.elementSize(
+                          screenHeight, 25, 25, 26, 26, 28, 36, 38, 40),
+                    )),
+                onTap: () => _service.sendSms(mule.phoneNumber),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
