911dc827c58196ca3cc9443a91917d75cf3a25a0
Zohar Cochavi <zohar.cochavi@gmail.com>
2021-02-13 14:43:04 +0100

    Adds abstract panel

--- lib/screens/home/slider/panel.dart
+++ lib/screens/home/slider/panel.dart
@@ -1,22 +1,84 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mule/screens/home/map/map_widget.dart';
 import 'package:mule/screens/home/slider/sliding_up_widget.dart';
 import 'package:mule/widgets/stylized_button.dart';
 
 abstract class Panel extends StatefulWidget {
-  final SlidingUpWidgetController slidingUpWidgetController = null;
-  final MapController mapController = null;
-  final double radius = null;
-  final double minHeight = null;
-  final double maxHeight = null;
-  final double buttonSpacing = null;
-  final double buttonSize = null;
-  final double _minHeight = null;
-  final double _maxHeight = null;
-  final bool _isDraggable = null;
-  final bool _backdropTapClosesPanel = null;
-  final double _backdropOpacity = null;
-  final List<StylizedButton> _buttonList = null;
-  final Function _mapStateCallback = null;
+  final SlidingUpWidgetController slidingUpWidgetController;
+  final MapController mapController;
+  final PanelController controller;
+  final double radius;
+  final double buttonSpacing;
+  final double buttonSize;
+  final double screenHeight;
+  final double backdropOpacity;
+  final bool backdropTapClosesPanel;
+
+  Panel({
+    @required this.slidingUpWidgetController,
+    @required this.mapController,
+    @required this.controller,
+    @required this.screenHeight,
+    this.backdropTapClosesPanel = true,
+    this.backdropOpacity = 0.0,
+    this.radius = 20,
+    this.buttonSize = 50.0,
+    this.buttonSpacing = 20.0,
+    Key key,
+  }) : super(key: key);
+
+  void init(State<Panel> panelState) {
+    initController(panelState);
+  }
+
+  void initController(State<Panel> panelState) {
+    controller._panelState = panelState;
+  }
+
+  double get maxHeight {
+    return screenHeight - 120;
+  }
+
+  double get minHeight {
+    return screenHeight / 5;
+  }
+
+  void mapStateCallback();
+
+  List<StylizedButton> get buttons;
+}
+
+abstract class DraggablePanel {
+  bool isOpen = false;
+
+  void open();
+
+  void close();
+}
+
+class PanelController {
+  State<Panel> _panelState;
+
+  bool get isAttached {
+    return _panelState != null;
+  }
+}
+
+class DraggableController extends PanelController {
+  void open() {
+    (_panelState as DraggablePanel).open();
+  }
+
+  void close() {
+    (_panelState as DraggablePanel).close();
+  }
+
+  bool get isOpen {
+    return (_panelState as DraggablePanel).isOpen;
+  }
+
+  double get currentHeight {
+    if (isOpen) return _panelState.widget.maxHeight;
+    return _panelState.widget.minHeight;
+  }
 }
