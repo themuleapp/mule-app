4143f1c65416e3d11d55564b8cd6dc54c7c0b60b
zoharcochavi <zohar.cochavi@gmail.com>
2021-04-04 13:40:18 +0200

    Shows route between mule, pickup, and destination

--- lib/screens/home/map/map_widget.dart
+++ lib/screens/home/map/map_widget.dart
@@ -248,10 +248,13 @@ class _MapWidgetState extends State<MapWidget> {
     });
   }
 
-  _showPolyLines({PointLatLng origin, PointLatLng destination}) async {
+  _showPolyLines({PointLatLng origin, PointLatLng destination, List<PointLatLng> waypoints}) async {
     // Polylines should be cleared when not used or updating
     if (_polylineCoords != null && !_polylineCoords.isEmpty) {
       return;
+    }
+    if (waypoints == null) {
+      waypoints = [];
     }
     List<LatLng> polylineCoordinates = [];
 
@@ -266,20 +269,19 @@ class _MapWidgetState extends State<MapWidget> {
       );
     }
 
+    List<PolylineWayPoint> polylinewaypoints = [];
+    waypoints.forEach((e) => polylinewaypoints.add(PolylineWayPoint(location: "${e.latitude}, ${e.longitude}")));
+
     PolylineResult result = await polylinePoints?.getRouteBetweenCoordinates(
         ExternalApi.googleApiKey, origin, destination,
         travelMode: TravelMode.walking,
-        wayPoints: [
-          PolylineWayPoint(
-              location:
-                  "${GetIt.I.get<LocationStore>().currentLocation.lat},${GetIt.I.get<LocationStore>().currentLocation.lng}")
-        ]);
+        wayPoints: polylinewaypoints
+    );
     if (result.points.isNotEmpty) {
       // loop through all PointLatLng points and convert them
       // to a list of LatLng, required by the Polyline
-      result.points.removeLast();
-      result.points.removeLast();
-      result.points.forEach((PointLatLng point) {
+      List<PointLatLng> uniquePoints = result.points;
+      uniquePoints.forEach((PointLatLng point) {
         polylineCoordinates.add(LatLng(point.latitude, point.longitude));
       });
     }
@@ -477,18 +479,14 @@ class MapController {
       double triggerDistance) async {
     await _mapWidgetState._removePolyLines();
 
-    PointLatLng target;
-    PointLatLng origin =
-        PointLatLng(muleLocation.latitude, muleLocation.longitude);
-    if (_mapWidgetState.calculateDistance(muleLocation, place) <
-        triggerDistance) {
-      target = PointLatLng(place.latitude, place.longitude);
-    } else {
-      target = PointLatLng(destination.latitude, destination.longitude);
-    }
+    PointLatLng placePoint  = PointLatLng(place.latitude, place.longitude);
+    PointLatLng destinationPoint = PointLatLng(destination.latitude, destination.longitude);
+    PointLatLng mulePoint = PointLatLng(muleLocation.latitude, muleLocation.longitude);
+    
     await _mapWidgetState._showPolyLines(
-      origin: origin,
-      destination: target,
+      origin: placePoint,
+      destination: destinationPoint,
+      waypoints: [mulePoint],
     );
     await _mapWidgetState._setRouteMarkers();
     await _mapWidgetState._setRouteView(focusLocation: [muleLocation]);
